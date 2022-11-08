d96110d45673fc354ff065a46f04967d50d793cf
sl243 <slmiucse@gmail.com>
2022-11-08 16:03:00 +0600

    added react router environment variable

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDsUH4BHUMfTDErT7cmPEySVpD2RVGSN1U",
-  authDomain: "shamim-photography.firebaseapp.com",
-  projectId: "shamim-photography",
-  storageBucket: "shamim-photography.appspot.com",
-  messagingSenderId: "631601757519",
-  appId: "1:631601757519:web:74b6d2a13a1fead707993a"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
