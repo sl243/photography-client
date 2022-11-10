afd795299dab516b7c55cc16564802765aad5e1f
sl243 <slmiucse@gmail.com>
2022-11-10 11:28:06 +0600

    added second question and answer in blog

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -68,6 +68,35 @@ const Blog = () => {
                 <div>
                 </div>
             </div>
+            <div className='mt-10'>
+                <h1 className='text-4xl text-center text-white font-semibold'>What is JWT? How does it works?</h1>
+                <p className='text-xl mt-3'>
+                    <strong>JWT:</strong> JWT, or JSON Web Token, is an open standard used to share security information between two parties — a client and a server. Each JWT contains encoded JSON objects, including a set of claims. JWTs are signed using a cryptographic algorithm to ensure that the claims cannot be altered after the token is issued.
+                </p>
+                <p className='text-xl mt-3'>
+                    <strong> JWT Works:</strong> JWTs differ from other web tokens in that they contain a set of claims. Claims are used to transmit information between two parties. What these claims are depends on the use case at hand. For example, a claim may assert who issued the token, how long it is valid for, or what permissions the client has been granted.
+                </p>
+                <p className='text-xl mt-3'>
+                    A JWT is a string made up of three parts, separated by dots (.), and serialized using base64. In the most common serialization format, compact serialization, the JWT looks something like this: xxxxx.yyyyy.zzzzz.
+                </p>
+                <p className='text-xl mt-3'>
+                    Once decoded, you will get two JSON strings: <br></br>
+                    1. The header and the payload. <br></br>
+                    2. The signature.
+                </p>
+                <p className='text-xl mt-3'>
+                    The JOSE (JSON Object Signing and Encryption) header contains the type of token — JWT in this case — and the signing algorithm.
+                </p>
+                <p className='text-xl mt-3'>
+                    The payload contains the claims. This is displayed as a JSON string, usually containing no more than a dozen fields to keep the JWT compact. This information is typically used by the server to verify that the user has permission to perform the action they are requesting.
+                </p>
+                <p className='text-xl mt-3'>
+                    There are no mandatory claims for a JWT, but overlaying standards may make claims mandatory. For example, when using JWT as bearer access token under OAuth2.0, iss, sub, aud, and exp must be present. some are more common than others.
+                </p>
+                <p className='text-xl mt-3'>
+                    The signature ensures that the token hasn’t been altered. The party that creates the JWT signs the header and payload with a secret that is known to both the issuer and receiver, or with a private key known only to the sender. When the token is used, the receiving party verifies that the header and payload match the signature.
+                </p>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
+        <div className='mt-10'>
             <footer className="footer p-10 bg-base-200 text-base-content">
                 <div>
                     <span className="footer-title">Services</span>
